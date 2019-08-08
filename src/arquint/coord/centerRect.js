a4e5e97f11532f1f5c16e21b4a8fd020da8b3caa
Linard Arquint <arquint.linard@gmail.com>
2019-08-08 09:21:01 +0200

    switches from column assignment center to left

--- src/arquint/coord/centerRect.js
+++ src/arquint/coord/centerRect.js
@@ -5,7 +5,7 @@
 export default function() {
     const columnWidth = 10;
     const columnSeparation = 1;
-    let columnIndexAssignment = columnIndexAssignmentCenter;
+    let columnIndexAssignment = columnIndexAssignmentLeftSubtree;
     function coordSpread(layers, columnWidthFunction, columnSeparationFunction) {
       // assign an index to each node indicating the "column" in which it should be placed
       columnIndexAssignment(layers);
@@ -23,7 +23,7 @@ export default function() {
             node.x0 = getColumnStartCoordinate(columnWidth, columnSeparation, node.columnIndex);
             node.x1 = node.x0 + columnWidth[node.columnIndex];
           });
-          return layer[layer.length - 1].x1;
+          return Math.max(...layer.map((node) => node.x1));
         })
       );
       layers.forEach((layer) => {
@@ -59,6 +59,52 @@ export default function() {
         const startColumnIndex = Math.floor((maxNodesPerLayer - nodesInLayer) / 2);
         layer.forEach((node, nodeIndex) => node.columnIndex = startColumnIndex + nodeIndex);
       });
+    }
+
+    function columnIndexAssignmentCenterSubtree(layers) {
+      columnIndexAssignmentSubtree(layers, true);
+    }
+
+    function columnIndexAssignmentLeftSubtree(layers) {
+      columnIndexAssignmentSubtree(layers, false);
+    }
+
+    function columnIndexAssignmentSubtree(layers, center) {
+      // keeps order of nodes in a layer but assigns columns to nodes in a layer based on their subtrees
+      if (layers.length == 0) {
+        return;
+      }
+      
+      // iterate over each node in the first layer and assign column indices to each node in its subtree.
+      // if a node already has a columnIndex, do not change it, this case can occur if the node has more than one predecessor
+      let startColumnIndex = 0;
+      layers[0].forEach((node) => {
+        const subtreeWidth = getSubtreeWidth(node);
+        node.columnIndex = startColumnIndex + (center ? Math.floor((subtreeWidth - 1) / 2) : 0);
+        assignColumnIndexToChildren(node, startColumnIndex);
+        startColumnIndex += subtreeWidth;
+      });
+
+      function getSubtreeWidth(node) {
+        if (node.children.length === 0) {
+          return 1;
+        }
+        return node.children.reduce((prevVal, child) => prevVal + getSubtreeWidth(child), 0);
+      }
+
+      function assignColumnIndexToChildren(node, startColumnIndex) {
+        const widthPerChild = node.children.map(getSubtreeWidth);
+        let childColumnIndex = startColumnIndex;
+        node.children.forEach((child, index) => {
+          if (child.columnIndex !== undefined) {
+            // stop recursion, this child was already visited
+            return;
+          }
+          child.columnIndex = childColumnIndex + (center ? Math.floor((widthPerChild[index] - 1) / 2) : 0);
+          assignColumnIndexToChildren(child, childColumnIndex);
+          childColumnIndex += widthPerChild[index];
+        });
+      }
     }
   }
   
